0162bb111d8e05d994a03a317a34bf5bb79fc84a
shalinirawat <shalinirawat11.10@gmail.com>
2022-12-11 23:07:17 +0530

    Discount APi and grpc changes

---
+++ src/Services/Discount/DiscountAPI/Program.cs
@@ -0,0 +1,32 @@
+using DiscountAPI.Extensions;
+using DiscountAPI.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//migrate PostGre database
+app.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Services/Discount/DiscountGrpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DiscountGrpc.Entities;
+using DiscountGrpc.Protos;
+
+namespace DiscountGrpc.Mapper
+{
+    public class DiscountProfile: Profile
+    {
+        public DiscountProfile() { 
+        CreateMap<Coupon,CouponModel>().ReverseMap();
+        }
+    }
+}

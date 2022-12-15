c63831e493800eadf1f3d86204d86e9d5703af04
shalinirawat <shalinirawat11.10@gmail.com>
2022-12-16 00:08:22 +0530

    API gateway changes

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,9 +1,21 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings=>settings.WithDictionaryHandle());
+
+
+//To Add ocelot.json file
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",true,true);
+
+//var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+//builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile($"ocelot.{environment}.json", optional: false, reloadOnChange: true)
+//    .AddEnvironmentVariables();
+
 builder.Host.ConfigureLogging(logging =>
 {
     logging.ClearProviders();
